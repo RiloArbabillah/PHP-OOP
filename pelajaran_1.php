22043a383276bc7e93023ecf8ab94b5cb9192feb
RiloArbabillah <rilo.arbabillah@gmail.com>
2022-11-21 11:03:00 +0800

    update

--- pelajaran_1.php
+++ pelajaran_1.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: rgba(0, 0, 0, 0.6); color:white">
+
 <?php
 echo "<a href='/'>Halaman Index</a> <br> <br>";
 // OOP merupakan metode pemrograman yang lebih berorientasi pada objek
@@ -57,4 +67,14 @@ echo $manusia->tampilkan_nama();
 // memanggil method warna_kulit dari class manusia
 echo $manusia->warna_kulit();
 
-echo "<br> <br> <br>". $manusia->tampilkan_nama();
+echo '<br> <br>';
+var_dump($manusia);
+echo '<br> <br>';
+
+for ($i=0; $i < 5 ; $i++) { 
+    for ($j=0; $j <= $i ; $j++) { 
+        echo "*";
+    }
+    echo '<br>';
+
+}
